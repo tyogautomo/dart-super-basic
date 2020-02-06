0a766ca3e88c2f3e7c3566529075747ab1e87a47
tyogautomo <yoga.utomo27@gmail.com>
2020-02-06 11:41:25 +0700

    OOP is good AF

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,22 @@
 import 'package:dart_super_basic/dart_super_basic.dart' as dart_super_basic;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_super_basic.calculate()}!');
+void main() {
+  User yoga = User('yoga');
+  User danang = User('danang');
+  print(yoga.login());
+}
+
+class User {
+  String username = '';
+  int age = 25;
+
+  User(this.username);
+
+  String greeting() {
+    return 'Hello my name is ${username} and i\'m ${age} years old.';
+  }
+
+  String login() {
+    return '${username} is logged in';
+  }
 }
