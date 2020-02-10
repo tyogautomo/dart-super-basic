47ba9f76204ffb552d66776efa0e846d58a2d585
tyogautomo <yoga.utomo27@gmail.com>
2020-02-10 10:55:36 +0700

    final lesson

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,26 @@
 import 'package:dart_super_basic/dart_super_basic.dart' as dart_super_basic;
 
 void main() {
-  User yoga = User('yoga');
-  User danang = User('danang');
+  User yoga = User('yoga', 25);
+  User danang = User('danang', 30);
+  SuperUser newUser = SuperUser('orangsuper', 33);
+
   print(yoga.login());
+  print(danang.greeting());
+  print(yoga.greeting());
+  print('yang super namanya ${newUser.username}, umurnya ${newUser.age}');
+  newUser.publish();
+  print(newUser.login());
 }
 
 class User {
-  String username = '';
-  int age = 25;
+  String username;
+  int age;
 
-  User(this.username);
+  User(String username, int age) {
+    this.username = username;
+    this.age = age;
+  }
 
   String greeting() {
     return 'Hello my name is ${username} and i\'m ${age} years old.';
@@ -18,5 +28,13 @@ class User {
 
   String login() {
     return '${username} is logged in';
+  }
+}
+
+class SuperUser extends User {
+  SuperUser(String username, int age) : super(username, age);
+
+  void publish() {
+    print('published successfuly');
   }
 }
